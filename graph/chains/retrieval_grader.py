0a62b72ff2eedb089665943847c27a12d70226f2
Buğrahan Öztürk <bugrahanoz061@gmail.com>
2026-01-20 14:01:28 +0300

    feat: add some graph nodes

---
+++ graph/chains/retrieval_grader.py
@@ -0,0 +1,16 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, Field
+from typing import Literal
+
+llm = ChatOpenAI(temperature=0)
+
+class GradeDocuments(BaseModel):
+    """Binary score for relevance check on retrieved documents"""
+
+    binary_score : str = Field(
+        description="Documents are relevant to the question, 'yes' or 'no'"
+    )
+
+structured_llm_grader = llm.with_structured_output(GradeDocuments)
+
